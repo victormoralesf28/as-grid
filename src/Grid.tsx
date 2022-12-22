01cba1b96132ddf6fb950f9c327e572b404c79ff
Victor Morales <victor.morales@beauthentic.digital>
2022-12-21 19:00:50 -0500

    Task 1

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -3,6 +3,7 @@ import { ColDef } from "ag-grid-community";
 import data from "./near-earth-asteroids.json";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
+import "./Grid.css";
 
 const columnDefs: ColDef[] = [
   { field: "designation", headerName: "Designation" },
@@ -14,17 +15,26 @@ const columnDefs: ColDef[] = [
   { field: "period_yr", headerName: "Period (yr)" },
   { field: "i_deg", headerName: "Inclination (deg)" },
   { field: "pha", headerName: "Potentially Hazardous" },
-  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, },
+  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true },
 ];
 
 const NeoGrid = (): JSX.Element => {
   return (
-    <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
-      <AgGridReact
-        rowData={data}
-        columnDefs={columnDefs}
-        rowGroupPanelShow={'always'}
-      />
+    <div>
+      <h1>Near-Earth Object Overview</h1>
+      <div
+        className="ag-theme-alpine"
+        style={{ height: "calc(100vh - 64px)", width: "100%" }}
+      >
+        <AgGridReact
+          rowData={data}
+          columnDefs={columnDefs}
+          gridOptions={{
+            animateRows: true,
+          }}
+          rowGroupPanelShow={"always"}
+        />
+      </div>
     </div>
   );
 };
